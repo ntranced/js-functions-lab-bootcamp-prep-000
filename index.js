eb236d0abfebde6ce2725158d05d55e34fe19b9a
Andrew Nguyen <andrew.ntranced@gmail.com>
2018-08-30 21:36:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function happyHolidays () {
 }
 
 function happyHolidaysTo(name) {
-  return('Happy holidays, ${name}!')
+  return(`Happy holidays, ${name}!`)
 }
 
 function happyHolidayTo(holiday, name) {
-  return('Happy ${holiday}, ${name}!')
+  return(`Happy ${holiday}, ${name}!`)
 }
